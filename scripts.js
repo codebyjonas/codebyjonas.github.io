8e9a26514e9735d76d07e35ef8fd38aa871c4a99
Jonas Olaussen <jonasolaussen@gmail.com>
2017-07-31 21:11:58 +0200

    Slider Sliding

--- scripts.js
+++ scripts.js
@@ -1,68 +1,83 @@
 $(document).ready(function () {
-    
-  /* #####
-    Smooth Scroll
-  */
-  $("a").on('click', function (event) {
-      if (this.hash !== "") {
-          
-          $("nav").removeClass("open");
-        
-          event.preventDefault();
-          var hash = this.hash;
 
-          $('html, body').animate({
-              scrollTop: $(hash).offset().top 
-          }, 800, function () {
-              window.location.hash = hash;
-          });
-      } 
-  });
+    /* #####
+      Smooth Scroll
+    */
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
 
-  
-  
-  /* #####
-    Menu
-  */
-  
-  nav = $("nav");
-  
-  // Open menu
-  $(".menu-toggle").on('click', function (event){
-      nav.addClass("open");
-  });
-  
-  // Close menu
-  $("main").on('click', function (event){
-      nav.removeClass("open");
-  });
-  
-  /* #####
-    Info Container
-  */
-  allInfoContainers = $(".info-box-container");
-  
-  allInfoContainers.on('click', function(event){
-     allInfoContainers.removeClass("open");
-     $(this).addClass("open");
-  });
-  
-  /* #####
-    Tag-line-slider
-  */
-  $(".tag-line").hide();
-  
-  $(".tag-line-slider-object").each(function(index,element) {
-    console.log(index);
-    
+            $("nav").removeClass("open");
 
-      $(".tag-line-slider-object").hide('slide',{direction:'left'},1000).delay(500);
-     
-      $(this).show('slide', {direction: 'right'}, 1000).delay(3000);
-    
-  });
-  
-  
-    
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+
+
+    /* #####
+      Menu
+    */
+
+    nav = $("nav");
+
+    // Open menu
+    $(".menu-toggle").on('click', function (event) {
+        nav.addClass("open");
+    });
+
+    // Close menu
+    $("main").on('click', function (event) {
+        nav.removeClass("open");
+    });
+
+    /* #####
+      Info Container
+    */
+    allInfoContainers = $(".info-box-container");
+
+    allInfoContainers.on('click', function (event) {
+        allInfoContainers.removeClass("open");
+        $(this).addClass("open");
+    });
+
+    /* #####
+      Tag-line-slider
+    */
+    $(".tag-line").hide();
+
+    function tagLineSlider() {
+
+        var duration = 750;
+        var curr = 2;
+        var count = $(".tag-line-slider-object").length;
+
+        $(".tag-line-slider-object:nth-child(1)").show("slide", { direction: "left" }, duration);
+
+        setInterval(function () {
+
+            console.log(curr);
+
+            $(".tag-line-slider-object").hide("slide", { direction: "left"}, duration);
+
+            $(".tag-line-slider-object:nth-child("+curr+")").delay(750).show("slide", { direction: "left" }, duration);
+            
+            curr++;
+            
+            if(curr == count + 1){
+                curr = 1;
+            }
+        }, 3000);
+    }
+
+    tagLineSlider();
+
+
 
 });
