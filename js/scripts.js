0b29e62ffd52abcc64bc55b56fa0bc31cdd058f5
jonasolaussen <jonasolaussen@gmail.com>
2017-08-03 16:52:51 -0400

    Added form  validation etc

--- js/scripts.js
+++ js/scripts.js
@@ -1,130 +1,226 @@
-$(function () {
+/* #####
+  Smooth Scroll
+*/
+function ActiveSmoothScroll() {
 
-    /* #####
-      Smooth Scroll
-    */
-    $("a").on('click', function (event) {
-        if (this.hash !== "") {
+  $("a").on('click', function(event) {
+    if (this.hash !== "") {
 
-            $("nav").removeClass("open");
+      $("nav").removeClass("open");
 
-            event.preventDefault();
-            var hash = this.hash;
+      event.preventDefault();
+      var hash = this.hash;
 
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 800, function () {
-                window.location.hash = hash;
-            });
-        }
-    });
-
-
-
-    /* #####
-      Menu
-    */
-
-    nav = $("nav");
-
-    // Open menu
-    $(".menu-toggle").on('click', function (event) {
-        nav.addClass("open");
-    });
-
-    // Close menu
-    $("main").on('click', function (event) {
-        nav.removeClass("open");
-    });
-
-    /* #####
-      Info Container
-    */
-    allInfoContainers = $(".info-box-container");
-
-    allInfoContainers.on('click', function (event) {
-        allInfoContainers.removeClass("open");
-        $(this).addClass("open");
-    });
-
-    /* #####
-      Tag-line-slider
-    */
-    $(".tag-line").hide();
-
-    function tagLineSlider() {
-
-        var duration = 750;
-        var curr = 2;
-        var count = $(".tag-line-slider-object").length;
-
-        $(".tag-line-slider-object:nth-child(1)").show("slide", { direction: "left" }, duration);
-
-        setInterval(function () {
-
-            $(".tag-line-slider-object").hide("slide", { direction: "left" }, duration);
-
-            $(".tag-line-slider-object:nth-child(" + curr + ")").delay(750).show("slide", { direction: "left" }, duration);
-
-            curr++;
-
-            if (curr == count + 1) {
-                curr = 1;
-            }
-        }, 3000);
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function() {
+        window.location.hash = hash;
+      });
     }
+  });
+}
 
-    tagLineSlider();
 
-    /* #####
-        Start Stellar
-    */
-    $(window).stellar({
-        horizontalScrolling: false,
-        verticalOffset: 0,
-        horizontalOffset: 0,
 
+/* #####
+  Menu
+*/
+function ActiveMenu() {
+
+
+  var nav = $("nav");
+
+  // Open menu
+  $(".menu-toggle").on('click', function(event) {
+    nav.addClass("open");
+  });
+
+  // Close menu
+  $("main").on('click', function(event) {
+    nav.removeClass("open");
+  });
+}
+
+/* #####
+  Info Container
+*/
+function infoContainers() {
+  allInfoContainers = $(".info-box-container");
+
+  allInfoContainers.on('click', function(event) {
+    allInfoContainers.removeClass("open");
+    $(this).addClass("open");
+  });
+}
+
+
+/* #####
+  Tag-line-slider
+*/
+
+
+function tagLineSlider() {
+
+  var duration = 750;
+  var curr = 2;
+  var count = $(".tag-line-slider-object").length;
+
+  $(".tag-line-slider-object:nth-child(1)").show("slide", {
+    direction: "left"
+  }, duration);
+
+  setInterval(function() {
+
+    $(".tag-line-slider-object").hide("slide", {
+      direction: "left"
+    }, duration);
+
+    $(".tag-line-slider-object:nth-child(" + curr + ")").delay(750).show("slide", {
+      direction: "left"
+    }, duration);
+
+    curr++;
+
+    if (curr == count + 1) {
+      curr = 1;
+    }
+  }, 3000);
+}
+
+
+
+
+/* #####
+    Waypoints
+*/
+function startWaypoints(){  
+
+  $(".grid-container").css({
+    "opacity": "0"
+  });
+
+  $("#about").waypoint(function(direction) {
+    $("#about .grid-container").css({
+      "opacity": "1"
     });
+  }, {
+    offset: '75%'
+  })
 
-    /* #####
-        Waypoints
-    */
+  $("#references").waypoint(function(direction) {
+    $("#references .grid-container").css({
+      "opacity": "1"
+    });
+  }, {
+    offset: '75%'
+  })
 
-    $(".grid-container").css({ "opacity": "0" });
+  $("#contact").waypoint(function(direction) {
+    $("#contact .grid-container").css({
+      "opacity": "1"
+    });
+  }, {
+    offset: '75%'
+  })
+}
 
-    $("#about").waypoint(function (direction) {
-        $("#about .grid-container").css({ "opacity": "1" });
-    }, {
-            offset: '75%'
-        })
+/* #####
+  Validate Form Data
+*/
+function validateForm(){
+  
+  var emailOk = false;
+  var msgOk = false;
+  
+  
+  //Email
+  function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+  }
+  
+  function isEverythingOk(){
+    if(emailOk && msgOk){
+      $("#submitForm").removeClass("disable");
+    } else{
+       $("#submitForm").addClass("disable");
+    }
+  }
+  
+  $("input#email").keyup(function(e){
+    
+    if(validateEmail($(this).val())){
+       $(this).parent().removeClass("input-non-valid").addClass("input-valid");
+       emailOk = true;
+    } else {
+        $(this).parent().removeClass("input-valid").addClass("input-non-valid");
+        emailOK = false;
+    }
+    
+    isEverythingOk();
+    
+  });
+  
+   $("input#email").keyup(function(e){
+  
+  });
+}
 
-    $("#references").waypoint(function (direction) {
-        $("#references .grid-container").css({ "opacity": "1" });
-    }, {
-            offset: '75%'
-        })
+/* #####
+  Submit Form Data
+*/
+function submitFormData() {
+  var email = $("#email").val();
+  var msg = $("#message").val();
 
-    $("#contact").waypoint(function (direction) {
-        $("#contact .grid-container").css({ "opacity": "1" });
-    }, {
-            offset: '75%'
-        })
+  $.post("https://formspree.io/jonasolaussen@gmail.com", {
+      email: email,
+      msg: msg
+    });
+}
 
-    /* #####
-        Fade In Content
-    */
-    $(".text-container").hide();
+/* #####
+  Start Functions
+*/
+$(function() {
+  ActiveSmoothScroll();
+  ActiveMenu();
+  infoContainers();
+  startWaypoints();
+  validateForm();
+  
+  $(".tag-line").hide();
+  tagLineSlider();
+
+  /* #####
+    Start Stellar
+*/
+  $(window).stellar({
+    horizontalScrolling: false,
+    verticalOffset: 0,
+    horizontalOffset: 0,
+
+  });
+  
+  /* #####
+    Fade In Content
+*/
+$(".text-container").hide();
+
+  $("#submitForm").on('click', function() {
+    submitFormData();
+
+  });
+
 });
 
-$(window).load(function () {
+$(window).load(function() {
 
-    /* #####
-        Fade In Content
-    */
-    setInterval(function () {
-        $(".text-container").fadeIn();
-    }, 500);
+  /* #####
+      Fade In Content
+  */
+  setInterval(function() {
+    $(".text-container").fadeIn();
+  }, 500);
 
 });
-
-
